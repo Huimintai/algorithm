a638212e286a509602b3acdb0e69c10fd11f7dab
Huimintai <1205913055@qq.com>
2016-06-20 22:57:20 -0400

    add code

--- myC++/crossNum.cpp
+++ myC++/crossNum.cpp
@@ -6,36 +6,53 @@ bool cmp(int a, int b)
 {
 	return a<b;
 }
-void crossNum(char* input,int len,  char* output)
+void crossNum(int* input,int len,  int* output)
 {
 	sort(input, input+len, cmp);
-	char* p = output;
+	int* odd = new int(len);
+	int* even = new int(len);
+	int oddNum = 0;
+	int evenNum = 0;
 	for(int i=0; i<len; i++)
 	{
-		if(input[i] % 2 != 0)
-		{
-			*p = input[i];
-			p +=2;
-		}
+		input[i]%2==0? (even[evenNum++] = input[i]) : (odd[oddNum++] = input[i]);
 	}
-	p = output;
-	p++;
-	for(int i=0; i<len; i++)
+	for(int i=0; i<evenNum; i++)
 	{
-		if(input[i] % 2 == 0)
-		{
-			*p = input[i];
-			p += 2;
-		}
+		cout<<even[i]<<" ";
 	}
+	cout<<endl;
+	for(int i=0; i<oddNum; i++)
+        {
+                cout<<odd[i]<<" ";
+        }
+	cout<<endl;
+	int oNum = 0;
+	int eNum = evenNum - 1;
+	int out = 0;
+	while(oNum<oddNum && eNum>=0)
+	{
+		output[out++] = odd[oNum++];
+		output[out++] = even[eNum--];
+	}
+	while(eNum >= 0)
+	{
+		output[out++] = even[eNum--];
+	}
+	while(oNum < oddNum)
+	{
+		output[out++] = odd[oNum++];
+	}
+//	delete [] odd;
+//	delete [] even;
 }
 
 int main()
 {
-	char a[10] = {1,2,3,4,5,6,7,8,9,10};
-	char b[20] = {'\0'};
-	crossNum(a, 10, b);
-	for(int i=0; i<20; i++)
+	int a[12] = {1,2,3,4,5,6,7,8,9,10,11,13};
+	int b[12];
+	crossNum(a, 12, b);
+	for(int i=0; i<12; i++)
 	{
 		cout<<b[i]<<" ";
 	}
