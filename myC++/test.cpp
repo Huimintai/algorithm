710b8f1781ac2337a678c8dd1c50918bfb5a6e12
Huimintai <1205913055@qq.com>
2016-06-24 01:14:34 -0400

    update

--- myC++/test.cpp
+++ myC++/test.cpp
@@ -1,19 +1,48 @@
-#include<stdio.h>
-int  main() {
-//	int a=2,x=2,p,q;
-//	q=(++x)+(++x);
-//	printf("%d,%d\n",x,q);
-	
-//	int b = 3;
-//	 int arr[] = {6,7,8,9,10};
-//	 int *ptr = arr;
-//	 *(ptr++)+=123;
-//	 printf("%d,%d\n",*ptr,*(++ptr));
-	
-//	int b = 3;
-	 int arr[] = {6,7,8,9,10};
-	 int *ptr = arr;
-	 *(ptr++)+=123;
-	 printf("%d,%d\n",*ptr,*ptr++);
-	 printf("%d\n", *ptr);
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+void crossNum(int* input, int len, int* output)
+{
+	sort(input, input+len);
+	int* odd = new int(len);
+	int* even = new int(len);
+	int oddNum = 0;
+	int evenNum = 0;
+	for(int i=0; i<len; i++)
+	{
+		input[i]%2==0 ? even[evenNum++]=input[i]: odd[oddNum++]=input[i];
+	}
+	int oNum = 0;
+	int eNum = evenNum - 1;
+	int num = 0;
+	while(oNum<oddNum && eNum>=0)
+	{
+		output[num++] = odd[oNum++];
+		output[num++] = even[eNum--];
+	}
+	while(num<len)
+	{
+		if(oNum==oddNum)
+		{
+			output[num++] = even[eNum--];
+		}
+		else
+		{
+			output[num++] = odd[oNum++];
+		}
+	}
+}
+
+int main()
+{
+	int a[12] = {1,2,3,4,5,6,7,8,9,10,12,14};
+	int b[12];
+	crossNum(a,12,b);
+	for(int i=0; i<12; i++)
+	{
+		cout<<b[i]<<" ";
+	}
+	cout<<endl;
 }
